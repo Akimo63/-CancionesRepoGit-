4208669f960afcd42c8981e49bc80e57a166dc3c
Akimo <Akimo@DESKTOP-HE2EPFA>
2022-03-23 11:34:49 +0100

    Creacion de new proyect

---
+++ src/ejercicioClaraEntorno_1/Cancion.java
@@ -0,0 +1,174 @@
+package ejercicioClaraEntorno_1;
+/**
+ * Es el metodo donde estan los constructores, los get, set y others
+ * @author Marco Alvarez
+ * @version 02.2022
+ * {@link Cancion#finalize()}
+ * {@link Cancion#isSonando()}
+ * {@link Cancion#MismaCancion(Cancion)}
+ * {@link Cancion#pararCancion()}
+ * {@link Cancion#reproducirCancion()}
+ * {@link Cancion#MayorDuracion(Cancion[])}
+ * {@link Cancion#Cancion()}
+ * {@link Cancion#Cancion(String, int)}
+ * {@link Cancion#Cancion(String, String, int, boolean)}
+ */
+public class Cancion {
+
+	private String titulo;
+	private String grupo;
+	private int duracion;
+	private boolean sonando;
+
+	// Definimos los constructores
+	/**
+	 * Es el constructor por defecto
+	 */
+	Cancion() // Constructor sin parámetros
+	{
+		this.titulo = "";
+		this.duracion = 0;
+		this.sonando = false;
+	}
+	/**
+	 * Este metodo sirve para crear una cancion con titulo duracion y si esta o no sonando
+	 * @param titulo, indica el titulo de la cancion
+	 * @param duracion, indica la duracion de la cancion
+	 */
+	Cancion(String titulo, int duracion) // constructor con 2 parámetros
+	{
+		this.titulo = titulo;
+		this.duracion = duracion;
+		this.sonando = false;
+	}
+	/**
+	 * Este metodo sirve para crear la cancion con titulo, grupo, duracion y si esta sonando
+	 * @param titulo, indica el titulo de la cancion
+	 * @param grupo, indica el grupo de la cancion
+	 * @param duracion, indica la duracion de la cancion
+	 * @param sonando, indica si esta sonando la cancion
+	 */
+	Cancion(String titulo, String grupo, int duracion, boolean sonando) // constructor con 4 parámetros
+	{
+		this.titulo = titulo;
+		this.duracion = duracion;
+		this.sonando = sonando;
+		this.grupo = grupo;
+	}
+	/**
+	 * Sirve para reproducir cancion
+	 * @return, indica si la cancion se esta ejecutando o no para poder reproducir la siguente
+	 */
+	boolean reproducirCancion() {
+		if (this.sonando) {
+			return false;
+		} else {
+			this.sonando = true;
+			return true;
+		}
+	}
+	/**
+	 * Sirve para parar la cancion
+	 * @return, indica si la cancion esta parada si es asi no hace nada si no pues la para
+	 */
+	boolean pararCancion() {
+		if (!this.sonando) {
+			return false;
+		} else {
+			this.sonando = false;
+			return true;
+		}
+	}
+
+	// meter métodos get/set/toString()
+	/**
+	 * devuelce el titulo de la cancion
+	 * @return
+	 */
+	public String getTitulo() {
+		return titulo;
+	}
+	/**
+	 * da el valor titulo a cancion
+	 * @param titulo
+	 */
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getGrupo() {
+		return grupo;
+	}
+
+	public void setGrupo(String grupo) {
+		this.grupo = grupo;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	public boolean isSonando() {
+		return sonando;
+	}
+
+	public void setSonando(boolean sonando) {
+		this.sonando = sonando;
+	}
+
+	boolean MismaCancion(Cancion cancion) {
+		if (this.titulo == cancion.titulo && this.grupo == cancion.grupo)
+			return true;
+		else
+			return false;
+	}
+	/**
+	 * sirve para saber la cancion mas larga
+	 * @param canciones, indica la cancion
+	 * @return, devuelve la cancion con mayor duracion
+	 */
+	public static String MayorDuracion(Cancion[] canciones) {
+		int posMax = 0;
+		int max = 0;
+		for (int i = 0; i < canciones.length; i++) {
+			if (canciones[i].duracion > max) {
+				posMax = i;
+				max = canciones[i].duracion;
+			}
+		}
+		return canciones[posMax].titulo;
+
+	}
+	/**
+	 * sirve para saber la siguente cancion
+	 * @param canciones, indica cual es la cancion
+	 * @param titulo, indica cual es el titulo de la siguente cancion
+	 * @return, duevuelve el titulo de la siguiente cancion
+	 */
+	static String SiguienteCancion(Cancion[] canciones, String titulo) {
+		int pos = 0;
+		int siguiente = 0;
+		for (int i = 0; i < canciones.length; i++) {
+			if (titulo == canciones[i].titulo) {
+				pos = i;
+			}
+		}
+		if (pos == canciones.length - 1) {
+			siguiente = 0;
+		} else {
+			siguiente = pos + 1;
+		}
+		return canciones[siguiente].titulo;
+	}
+	/**
+	 * imprime la cancion con su titulo autor grupo duracion y si esta sonando
+	 */
+	@Override
+	public String toString() {
+		return "Cancion [titulo=" + titulo + ", autor=" + grupo + ", duracion=" + duracion + ", sonando=" + sonando+ "]";
+	}
+}
---
+++ src/ejercicioClaraEntorno_1/PruebaCancion.java
@@ -0,0 +1,67 @@
+package ejercicioClaraEntorno_1;
+/**
+ * Metodo main
+ * @author Marco Alvarez
+ * @version 02.2022
+ *
+ */
+public class PruebaCancion {
+	/**
+	 * Sirve para testear cancion, los siguentes metodos son los usados en la clase main
+	 * @param args
+	 * {@link PruebaCancion#mostrarCancionesyAutor(Cancion[])}
+	 * {@link PruebaCancion#mostrarCancionMasLarga(Cancion[])}
+	 * {@link PruebaCancion#mostrarSiguienteCancion(Cancion, Cancion[])}
+	 * {@link PruebaCancion#PruebaCancion()}
+	 */
+	public static void main(String[] args) {
+		
+		Cancion cancionPrueba = new Cancion();
+		System.out.println(cancionPrueba);
+		
+		Cancion cancionClasico = new Cancion("Sweet Home Alabama", 180);
+		System.out.println(cancionClasico);
+		
+		Cancion primeraCancion = new Cancion("I Don't Like Mondays", "The Boomtown Rats", 390, true);
+		Cancion segundaCancion = new Cancion("Don’t Stop Believin’", "Journey", 320, false);
+		Cancion terceraCancion = new Cancion("Come Join The Murder", "The White Buffalo & The Forest Rangers", 420, false);
+
+		if (primeraCancion.reproducirCancion()) {
+			System.out.println(primeraCancion.getTitulo() + "está sonando");
+		}
+		if (primeraCancion.pararCancion()) {
+			System.out.println(primeraCancion.getTitulo() + "stop");
+		}
+		if (primeraCancion.MismaCancion(cancionClasico)) {
+			System.out.println("Parece que no son la misma canción");
+		}
+		Cancion nueva = new Cancion("Baba O'riley", "The Who", 310, false);
+		Cancion[] canciones = { primeraCancion, segundaCancion, terceraCancion, nueva };
+		
+		mostrarCancionesyAutor(canciones);
+		mostrarCancionMasLarga(canciones);
+
+		mostrarSiguienteCancion(primeraCancion, canciones);
+		mostrarSiguienteCancion(nueva, canciones);
+	}
+
+
+	private static void mostrarSiguienteCancion(Cancion miCancion, Cancion[] canciones) {
+		System.out.println("La canción que sonará después de " + miCancion.getTitulo() + " será "
+				+ Cancion.SiguienteCancion(canciones, miCancion.getTitulo()));
+	}
+
+
+	private static void mostrarCancionMasLarga(Cancion[] canciones) {
+		System.out.println("La canción más larga es: " + Cancion.MayorDuracion(canciones));
+	}
+
+
+	private static void mostrarCancionesyAutor(Cancion[] canciones) {
+		for (int i = 0; i < canciones.length; i++) {
+			System.out.println("La canción es " + canciones[i].getTitulo() + " de " + canciones[i].getGrupo());
+		}
+	}
+	
+
+}
